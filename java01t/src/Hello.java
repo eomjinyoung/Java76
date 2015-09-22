8153ddb2be9c7cd608ce8ba522c6fbb5691b56d8
Jinyoung <jinyoung.eom@gmail.com>
2015-09-22 11:30:49 +0900

    .

--- java01t/src/Hello.java
+++ java01t/src/Hello.java
@@ -5,5 +5,6 @@ public class Hello {
     System.out.println("Hello, World!"); 
     System.out.println("Hello, World!"); 
     System.out.println("Hello, World!");
+    System.out.println("ø¿»£∂Û...");
   } 
 }
