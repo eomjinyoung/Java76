3a271c22aa1040d4c31919b36d214e71dea30707
Jinyoung <jinyoung.eom@gmail.com>
2015-09-30 10:38:28 +0900

    .

--- java02t/src/step02/Exam09.java
+++ java02t/src/step02/Exam09.java
@@ -9,11 +9,11 @@ public class Exam09 {
     int j = 3;  // 00000011
     
     
-    //1) AND 비트 연산자
+    //1) AND(&) 비트 연산자
     //- 일부 데이터를 0으로 막는다.(예: 그림의 일부 영역에 마스크를 씌움)
     System.out.println(Integer.toBinaryString(i & j));
     
-    //2) OR 비튼 연산자
+    //2) OR(|) 비튼 연산자
     //- 일부 데이터를 1 증가시킨다.(예: 색상을 더 밝게...)
     System.out.println(Integer.toBinaryString(i | j));
     
@@ -47,7 +47,17 @@ public class Exam09 {
       System.out.println("출석하였습니다.");
     }
     
+    //Exclusive OR(^) 연산자: 다른 데이터와 다른 경우 특정 값으로 설정할 때 사용.
+    i = 10; // 00001010
+    j = 3;  // 00000011
+    //------------------
+    //         00001001
     
+    System.out.println(Integer.toBinaryString(i ^ j));
+    
+    //NOT(~) 비트연산자: 반전 효과에 주로 사용
+    i = 10; // 00001010
+    System.out.println(Integer.toBinaryString(~i));
   }
 }
 
