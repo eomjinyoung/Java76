794b9f84d6e3f6310ca43ebba6e532e90c1a18d0
Jinyoung <jinyoung.eom@gmail.com>
2015-10-15 14:10:08 +0900

    .

---
+++ project01/src/v01/LinkedList.java
@@ -0,0 +1,111 @@
+package v01;
+
+public class LinkedList {
+  private Bucket start;
+  private Bucket end;
+  private int length;
+  
+  public LinkedList() {
+    start = new Bucket();
+    end = start;
+  }
+  
+  public int add(Object value) {
+    end.setValue(value);
+    end.setNext(new Bucket());
+    end = end.getNext();
+    length++;
+    return 0;
+  }
+  
+  public Object get(int index) {
+    if (index < 0 || index >= length)
+      return null;
+    
+    int pos = 0;
+    Bucket cursor = start;
+    
+    while (pos < index) {
+      cursor = cursor.getNext();
+      pos++;
+    }
+    
+    return cursor.getValue();
+  }
+  
+  public Object remove(int index) {
+    if (index < 0 || index >= length)
+      return null;
+    
+    Object removedValue = null;
+    length--;
+    
+    if (index == 0) {
+      removedValue = start.getValue();
+      start = start.getNext();
+      return removedValue;
+    }
+    
+    int pos = 0;
+    Bucket cursor = start;
+    while (pos < index-1) {
+      cursor = cursor.getNext();
+      pos++;
+    }
+    
+    removedValue = cursor.getNext().getValue();
+    cursor.setNext(cursor.getNext().getNext());
+    
+    return removedValue;
+  }
+  
+  public int size() {
+    return length;
+  }
+  
+  public static void main(String[] args) {
+    LinkedList list = new LinkedList();
+    list.add("00000");
+    list.add("11111");
+    list.add("22222");
+    list.add("33333");
+    list.add("44444");
+    list.add("55555");
+ 
+    for (int i = 0; i < list.size(); i++) {
+      System.out.println(list.get(i));
+    }
+    System.out.println("----------------");
+    
+    System.out.println(list.remove(2));
+    System.out.println("----------------");
+    
+    for (int i = 0; i < list.size(); i++) {
+      System.out.println(list.get(i));
+    }
+    System.out.println("----------------");
+    
+    list.add("66666");
+    list.remove(0);
+    System.out.println("----------------");
+    
+    for (int i = 0; i < list.size(); i++) {
+      System.out.println(list.get(i));
+    }
+    
+    list.remove(4);
+    System.out.println("----------------");
+    
+    for (int i = 0; i < list.size(); i++) {
+      System.out.println(list.get(i));
+    }
+    
+  }
+}
+
+
+
+
+
+
+
---
+++ project01/src/v01/ProjectControl.java
@@ -0,0 +1,114 @@
+package v01;
+
+import java.sql.Date;
+import java.util.Scanner;
+
+public class ProjectControl {
+  Scanner scanner;
+  LinkedList projects = new LinkedList();
+  
+  public ProjectControl(Scanner scanner) {
+    this.scanner = scanner;
+  }
+  
+  public void service() {
+    String command = null;
+    do {
+      System.out.print("프로젝트관리> ");
+      command = scanner.nextLine(); 
+      switch (command) {
+      case "list":
+        doList();
+        break;
+      case "add":
+        doAdd();
+        break;
+      case "delete":
+        doDelete();
+        break;  
+      case "help":
+        doHelp();
+        break;
+      case "main":
+        return;
+      default:
+        System.out.println("해당 명령을 지원하지 않습니다.");
+      }
+    } while (!command.equals("quit"));
+  }
+
+  private void doList() {
+    System.out.printf("%-3s %-20s %-10s %-10s %-40s\n", 
+        "No", "Title", "Start", "End", "Members");
+
+    Project project = null;
+    for (int i = 0; i < projects.size(); i++) {
+      project = (Project)projects.get(i);
+      if (project == null) // 배열의 항목이 null인 경우, 다음 항목으로 바로 이동.
+        continue;
+      System.out.printf("% 3d %-20s %3$tY-%3$tm-%3$td %4$s %5$-40s\n", 
+          i, 
+          project.getTitle(),
+          project.getStartDate(),
+          project.getEndDate(),
+          project.getMember());
+    }
+  }
+  
+  private void doAdd() {
+    Project project = new Project();
+    
+    System.out.print("프로젝트명? ");
+    project.setTitle(scanner.nextLine());
+    
+    System.out.print("시작일? ");
+    project.setStartDate(Date.valueOf(scanner.nextLine()));
+    
+    System.out.print("종료일? ");
+    project.setEndDate(Date.valueOf(scanner.nextLine()));
+
+    System.out.print("멤버? ");
+    project.setMember(scanner.nextLine());
+    
+    System.out.print("정말 저장하시겠습니까?(y/n)");
+    String yesno = scanner.nextLine();
+    if (yesno.toLowerCase().equals("y")) {
+      if (projects.add(project) == 0) { 
+        System.out.println("저장되었습니다.");
+      } else {
+        System.out.println("저장소가 모두 찼습니다.\n저장할 수 없습니다!");
+      }
+    } else {
+      System.out.println("취소하였습니다.");
+    }
+  }
+  
+  private void doDelete() {
+    System.out.print("프로젝트 번호? ");
+    int no = Integer.parseInt(scanner.nextLine());
+    
+    System.out.print("정말 삭제하시겠습니까?(y/n)");
+    String yesno = scanner.nextLine();
+    
+    if (yesno.toLowerCase().equals("y")) {
+      if (projects.remove(no) != null) { 
+        System.out.println("삭제하였습니다.");
+      } else {
+        System.out.println("유효하지 않은 번호입니다.");
+      }
+    } else {
+      System.out.println("취소하였습니다.");
+    }
+  }
+  
+  private void doHelp() {
+    System.out.println("[사용법]");
+    System.out.println("명령");
+    System.out.println();
+    System.out.println("[명령]");
+    System.out.println("list         프로젝트 목록을 리턴한다.");
+    System.out.println("add          프로젝트를 추가한다.");
+    System.out.println("delete       프로젝트를 삭제한다. ");
+    System.out.println("main         메인으로 이동한다.");
+  }
+}
