1bad69f606bf71a81a773f21920a6c3d9f0afb16
Jinyoung <jinyoung.eom@gmail.com>
2015-10-19 17:45:45 +0900

    .

--- java02t/src/step13/ex08/A.java
+++ java02t/src/step13/ex08/A.java
@@ -9,6 +9,9 @@ public class A {
   }
   
   void m2() {
+    /*A.*/i = 4000;
+    j = 300;
+    /*A.*/m1();
     System.out.println("m2()");
   }
   
@@ -21,8 +24,11 @@ public class A {
   
   class C {
     void display() {
+      /*A.*/i = 30000;
+      /*A.this.*/j = 3434;
+      /*A.*/B p = new /*A.*/B();
       A.m1();
-      m2(); // A.this 변수가 생략되었다. A.this안에 A의 인스턴스 주소가 들어있다.
+      /*A.this.*/m2(); // A.this 변수가 생략되었다. A.this안에 A의 인스턴스 주소가 들어있다.
     }
   }
 }
