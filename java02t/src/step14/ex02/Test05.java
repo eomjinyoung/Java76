996c1b50a62e4367645cddda5114c9f26cd3abb3
Jinyoung <jinyoung.eom@gmail.com>
2015-10-20 14:52:34 +0900

    .

---
+++ java02t/src/step14/ex02/Test05.java
@@ -0,0 +1,28 @@
+package step14.ex02;
+
+import java.io.File;
+
+public class Test05 {
+
+  public static void main(String[] args) throws Exception {
+    // File 도구 사용법
+    File file = new File("./bin");
+    
+    // 문제: 클래스 이름만 출력하라! 
+    // => .class 확장자는 출력하지 않는다.
+    // 예) Test04
+    displayDirectory(file);
+  }
+  
+  private static void displayDirectory(File file) {
+    File[] files = file.listFiles();
+    for (File f : files) {
+      if(f.getName().endsWith(".class"))
+        System.out.println(f.getName().replace(".class", ""));
+      if (f.isDirectory()) {
+        displayDirectory(f);
+      }
+    }
+  }
+
+}
