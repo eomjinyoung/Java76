ef4bcbe89f1dbe0df436ea8676dc10069079dca0
Jinyoung <jinyoung.eom@gmail.com>
2015-10-22 12:28:18 +0900

    .

---
+++ java02t/src/step14/ex10/Test01_in.java
@@ -0,0 +1,30 @@
+package step14.ex10;
+
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+
+public class Test01_in {
+
+  public static void main(String[] args) throws Exception {
+    // primitive type 데이터 읽기
+    FileInputStream sink = new FileInputStream("./test/ex10.test01.dat");
+    BufferedInputStream decorator = new BufferedInputStream(sink);
+    DataInputStream in = new DataInputStream(decorator);
+
+    String name = null;
+    int kor = 0, eng = 0, math = 0;
+
+    name = in.readUTF();
+    kor = in.readInt();
+    eng = in.readInt();
+    math = in.readInt();
+    
+    System.out.printf("%s,%d,%d,%d\n", name, kor, eng, math);
+
+    in.close();
+    decorator.close();
+    sink.close();
+  }
+
+}
---
+++ java02t/src/step14/ex10/Test01_out.java
@@ -0,0 +1,31 @@
+package step14.ex10;
+
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+
+public class Test01_out {
+  public static void main(String[] args) throws Exception {
+    // primitive type 데이터 출력하기
+    FileOutputStream sink = new FileOutputStream("./test/ex10.test01.dat");
+    BufferedOutputStream decorator = 
+        new BufferedOutputStream(sink);
+    DataOutputStream out = new DataOutputStream(decorator);
+    
+    //성적 데이터 출력하기
+    String name = "홍길동";
+    int kor = 100;
+    int eng = 90;
+    int math = 80;
+
+    out.writeUTF(name);
+    out.writeInt(kor);
+    out.writeInt(eng);
+    out.writeInt(math);
+    
+    out.close();
+    decorator.close();
+    sink.close();
+    System.out.println("실행 완료!");
+  }
+}
