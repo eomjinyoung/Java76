bb9609e702861e7d0e36c9c46488f23157e79819
Jinyoung <jinyoung.eom@gmail.com>
2015-10-28 14:55:53 +0900

    .

--- java02t/src/step16/ex07/client/ChatClient.java
+++ java02t/src/step16/ex07/client/ChatClient.java
@@ -7,30 +7,27 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class ChatClient {
-
   public static void main(String[] args) {
     try (
       Socket socket = new Socket("localhost", 8888);
-      BufferedReader in = new BufferedReader(
-            new InputStreamReader(
-                socket.getInputStream()));
-        
-      PrintStream out = new PrintStream(socket.getOutputStream()); 
+      BufferedReader in = 
+          new BufferedReader(new InputStreamReader(socket.getInputStream()));
+      PrintStream out = new PrintStream(socket.getOutputStream());
       Scanner keyboard = new Scanner(System.in);
-    ) {
+    ) 
+    {
       String message = null;
+      String result = null;
       
       do {
-        System.out.print("> ");
         message = keyboard.nextLine();
         out.println(message);
-        System.out.println(in.readLine());
+        result = in.readLine();
+        System.out.println(result);
       } while (!message.equals("quit"));
       
     } catch (Exception e) {
       e.printStackTrace();
     }
-
   }
-
 }
