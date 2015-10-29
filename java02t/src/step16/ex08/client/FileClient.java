acde6da017c33a1f7048162480d997183d37d0e4
Jinyoung <jinyoung.eom@gmail.com>
2015-10-29 10:55:50 +0900

    .

--- java02t/src/step16/ex08/client/FileClient.java
+++ java02t/src/step16/ex08/client/FileClient.java
@@ -1,5 +1,65 @@
 package step16.ex08.client;
 
-public class FileClient {
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.net.Socket;
 
+public class FileClient {
+  
+  private void transfer(String path) {
+    File file = new File(path);
+    if (!file.exists() || !file.isFile()) {
+      System.out.println("파일이 존재하지 않거나 디렉토리입니다.");
+      return;
+    }
+    
+    String filename = file.getName();
+    long filesize = file.length();
+    
+    System.out.printf("%s(%d)\n", filename, filesize);
+    
+    try (
+      Socket socket = new Socket("localhost", 8888);
+      DataInputStream in = new DataInputStream(
+          new BufferedInputStream(socket.getInputStream()));
+      DataOutputStream out = new DataOutputStream(
+          new BufferedOutputStream(socket.getOutputStream()));  
+      BufferedInputStream fileIn = new BufferedInputStream( 
+          new FileInputStream(file));
+    ) 
+    {
+      out.writeUTF(filename); // 바이트크기(2)UTF바이트배열(n)
+      out.writeLong(filesize);
+      
+      int b = 0;
+      while ((b = fileIn.read()) != -1) {
+        out.write(b);
+      }
+      out.flush();
+      
+      String message = in.readUTF(); 
+      if (message.equals("ok")) {
+        System.out.println("전송 완료!");
+      } else {
+        System.out.println("전송 실패:");
+        System.out.println(message);
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+  
+  public static void main(String[] args) {
+    if (args.length < 1) {
+      System.out.println("사용법: java step16.ex08.client.FileClient 파일경로");
+      return;
+    }
+    
+    FileClient client = new FileClient();
+    client.transfer(args[0]);
+  }
 }
--- java02t/src/step16/ex08/server/FileServer.java
+++ java02t/src/step16/ex08/server/FileServer.java
@@ -1,5 +1,71 @@
 package step16.ex08.server;
 
-public class FileServer {
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class FileServer {
+  String uploadPath;
+  
+  private void setFileDirectory(String uploadPath) {
+    this.uploadPath = uploadPath;
+  }
+
+  private void service() {
+    System.out.println("파일 서버 시작됨!");
+    
+    Socket socket = null;
+    DataInputStream in = null;
+    DataOutputStream out = null;
+    BufferedOutputStream fileOut = null;
+    
+    try (
+      ServerSocket ss = new ServerSocket(8888);
+    ) 
+    {
+      socket = ss.accept();
+      in = new DataInputStream(
+          new BufferedInputStream(socket.getInputStream()));
+      out = new DataOutputStream(
+          new BufferedOutputStream(socket.getOutputStream())); 
+      
+      String filename = in.readUTF();
+      long filesize = in.readLong();
+      System.out.printf("%s(%d)\n", filename, filesize);
+      
+      fileOut = new BufferedOutputStream(
+          new FileOutputStream(uploadPath + "/" + filename));
+      
+      for (int i = 0; i < filesize; i++) {
+        fileOut.write(in.read());
+      }
+      System.out.println("파일 수신 완료!");
+      
+      out.writeUTF("ok");
+      out.flush();
+      
+    } catch (Exception e) {
+      try {
+        out.writeUTF(e.getMessage());
+        out.flush();
+      } catch (Exception e2) {}
+      
+    } finally {
+      
+      try {in.close();} catch (Exception e) {}
+      try {out.close();} catch (Exception e) {}
+      try {socket.close();} catch (Exception e) {}
+      try {fileOut.close();} catch (Exception e) {}
+    }
+  }
+  
+  public static void main(String[] args) {
+    FileServer server = new FileServer();
+    server.setFileDirectory("./upload");
+    server.service();
+  }
 }
