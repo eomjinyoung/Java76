336d9d462e9114b773af32942aa0b1c02ef1e54d
Jinyoung <jinyoung.eom@gmail.com>
2015-11-20 15:19:16 +0900

    .

--- web01t/src/main/java/servlet/OtherServlet.java
+++ web01t/src/main/java/servlet/OtherServlet.java
@@ -1,10 +1,11 @@
-/* forward 테스트용 서블릿
+/* ServletRequest에 저장된 데이터 사용하기
  
 */ 
 package servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.StringWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -15,21 +16,27 @@ public class OtherServlet extends HttpServlet {
   private static final long serialVersionUID = 1L;
   
   @Override
-  public void doGet(
+  public void service(
       HttpServletRequest request, HttpServletResponse response) 
       throws IOException, ServletException {
     response.setContentType("text/plain;charset=UTF-8");
     PrintWriter out = response.getWriter();
-    out.println("GET: OtherServlet 실행됨.");
-  }
-  
-  @Override
-  public void doPost(
-      HttpServletRequest request, HttpServletResponse response) 
-      throws IOException, ServletException {
-    response.setContentType("text/plain;charset=UTF-8");
-    PrintWriter out = response.getWriter();
-    out.println("POST: OtherServlet 실행됨.");
+    out.println("오류가 발생했습니다.");
+    
+    // ServletRequest에 보관된 오류 정보를 꺼낸다.
+    Exception e = (Exception)request.getAttribute("error");
+    
+    // Data Sink Stream 클래스를 사용하여 오류 정보를 저장한다.
+    // => StringWriter : 자기가 준비한 메모리로 출력한다.
+    // => PrintWriter : StringWriter에 꼽아서 printStackTrace()에 넘긴다.
+    // => printStackTrace(tempOutput) : 
+    //           PrintWriter를 이용하여 StringWriter에 오류 정보를 출력한다.
+    StringWriter stringBuffer = new StringWriter();
+    PrintWriter tempOutput = new PrintWriter(stringBuffer);
+    e.printStackTrace(tempOutput);
+    
+    // StringWriter에 출력된 내용을 꺼내서 웹 브라우저로 전송한다.
+    out.println(stringBuffer.toString());
   }
  
 }
--- web01t/src/main/java/servlet/Servlet01.java
+++ web01t/src/main/java/servlet/Servlet01.java
@@ -1,4 +1,6 @@
-/* Refresh
+/* ServletRequest를 사용하여 forwarding 하는 동안 데이터 공유하기
+=> Servlet01에서 발생한 예외 정보를 ServletRequest를 통해 
+   OtherServlet에게 전달할 수 있다.
  
 */ 
 package servlet;
@@ -6,6 +8,7 @@ package servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -18,20 +21,23 @@ public class Servlet01 extends HttpServlet {
   public void doGet(
       HttpServletRequest request, HttpServletResponse response) 
       throws IOException, ServletException {
-    response.setContentType("text/plain;charset=UTF-8");
-    PrintWriter out = response.getWriter();
-    // 출력문을 호출하면, 데이터는 내부 버퍼(보통 8KB)에 보관된다.
-    // 즉시 클라이언트로 출력되는 것이 아니다.
-    // service()의 호출이 끝나거나, 강제로 flush()를 호출하게 되면
-    // 버퍼의 저장된 데이터가 클라이언트로 출력된다.
-    out.println("결과를 출력한다.");
-    out.println("그리고 3초 후에 daum.net 사이트를 요청하게 만든다.");
-    //out.flush();
+    try {
+      response.setContentType("text/plain;charset=UTF-8");
+      PrintWriter out = response.getWriter();
+      int a = Integer.parseInt(request.getParameter("a"));
+      int b = Integer.parseInt(request.getParameter("b"));
+      int result = a / b;
+      out.printf("%d / %d = %d\n", a, b, result);
+      
+    } catch (Exception e) {
+      RequestDispatcher rd = request.getRequestDispatcher(
+                                       "/step07/otherservlet");
+      // ServletRequest에 오류 정보를 저장한다.
+      request.setAttribute("error", e);
+      
+      rd.forward(request, response);
+    }
     
-    //응답 헤더에 리프래시 정보를 추가한다.
-    //만약, 위에서와 같이 flush()를 호출하여 강제로 클라이언트에게 응답을 했다면,
-    //다음 메서드 호출은 무시된다.
-    response.setHeader("Refresh", "3;url=http://www.daum.net");
   }
  
 }
