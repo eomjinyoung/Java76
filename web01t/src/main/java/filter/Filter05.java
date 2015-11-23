76627f2f66b1d78da93e97ade81a1026d5f453d3
Jinyoung <jinyoung.eom@gmail.com>
2015-11-23 16:21:10 +0900

    .

---
+++ web01t/src/main/java/filter/Filter05.java
@@ -0,0 +1,48 @@
+/* Filter 초기화 파라미터 사용하기
+ * 
+ */
+package filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class Filter05 implements Filter {
+  FilterConfig config;
+  
+  @Override
+  public void init(FilterConfig filterConfig) throws ServletException {
+    System.out.println("Filter04.init()");
+    this.config = filterConfig;
+  }
+
+  @Override
+  public void doFilter(
+      ServletRequest request, ServletResponse response, FilterChain chain)
+      throws IOException, ServletException {
+    System.out.println("Filter04.doFilter()... 전반 작업");
+    System.out.println(request.getRemoteAddr());
+    
+    //web.xml에 선언된 필터의 초기화 파라미터 값 꺼내기. 
+    System.out.println(config.getInitParameter("encoding"));
+    System.out.println(config.getInitParameter("name"));
+
+    // 다음 필터를 실행한다. 만약 다음 필터가 없다면, 서블릿이 실행된다.
+    chain.doFilter(request, response);
+    
+    System.out.println("Filter04.doFilter()... 후반 작업");
+    
+  }
+
+  @Override
+  public void destroy() {
+    System.out.println("Filter04.destroy()");
+    
+  }
+
+}
