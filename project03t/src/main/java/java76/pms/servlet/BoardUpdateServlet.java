9ccc5b673cae3751c22a75001a4ba8ddd29321a5
Jinyoung <jinyoung.eom@gmail.com>
2015-11-25 11:31:37 +0900

    .

--- project03t/src/main/java/java76/pms/servlet/BoardUpdateServlet.java
+++ project03t/src/main/java/java76/pms/servlet/BoardUpdateServlet.java
@@ -22,82 +22,88 @@ public class BoardUpdateServlet extends HttpServlet {
   protected void doGet(
       HttpServletRequest request, HttpServletResponse response) 
       throws ServletException, IOException {
-    
-    int no = Integer.parseInt(request.getParameter("no"));
-    
-    ApplicationContext iocContainer = 
-        (ApplicationContext)this.getServletContext()
-                                .getAttribute("iocContainer");
-    BoardDao boardDao = iocContainer.getBean(BoardDao.class);
-    Board board = boardDao.selectOne(no);
-    
-    response.setContentType("text/html;charset=UTF-8");
-    PrintWriter out = response.getWriter();
-    
-    out.println("<!DOCTYPE html>");
-    out.println("<html>");
-    out.println("<head>");
-    out.println("  <meta charset='UTF-8'>");
-    out.println("  <title>게시판-상세정보</title>");
-    out.println("</head>");
-    out.println("<body>");
-    out.println("<h1>게시물 정보</h1>");
-    
-    if (board != null) {
-      out.println("<form id='form1' action='update' method='post'>");
-      out.println("<table border='1'>");
-      out.println("<tr>");
-      out.println("  <th>번호</th>");
-      out.printf("  <td><input type='text' name='no' value='%d' readonly></td>\n", 
-          board.getNo());
-      out.println("</tr>");
-      out.println("<tr>");
-      out.println("  <th>제목</th>");
-      out.printf("  <td><input type='text' name='title' value='%s'></td>\n", 
-          board.getTitle());
-      out.println("</tr>");
-      out.println("<tr>");
-      out.println("  <th>내용</th>");
-      out.printf("  <td><textarea rows='10' name='content' cols='60'>%s</textarea></td>\n", 
-          board.getContent());     
-      out.println("</tr>");
-      out.println("<tr>");
-      out.println("  <th>조회수</th>");
-      out.printf("  <td>%d</td>\n", 
-          board.getViews());
-      out.println("</tr>");
-      out.println("<tr>");
-      out.println("  <th>등록일</th>");
-      out.printf("  <td>%s</td>\n", 
-          board.getCreatedDate());
-      out.println("</tr>");
-      out.println("<tr>");
-      out.println("  <th>암호</th>");
-      out.println("  <td><input type='password' name='password'></td>");
-      out.println("</tr>");
-      out.println("</table>");
+    try {
+      int no = Integer.parseInt(request.getParameter("no"));
       
-      out.println("<p>");
-      out.println("<button name='update' type='submit'>변경</button>");
-      out.println("<button name='delete' type='submit' onclick='deleteBoard()'>삭제</button>");
-      out.println("</p>");
+      ApplicationContext iocContainer = 
+          (ApplicationContext)this.getServletContext()
+                                  .getAttribute("iocContainer");
+      BoardDao boardDao = iocContainer.getBean(BoardDao.class);
+      Board board = boardDao.selectOne(no);
       
-      out.println("</form>");
+      response.setContentType("text/html;charset=UTF-8");
+      PrintWriter out = response.getWriter();
       
-    } else {
-      out.println("<p>해당 번호의 게시물을 찾을 수 없습니다.</p>");
-    }
-    RequestDispatcher rd = request.getRequestDispatcher("/copyright");
-    rd.include(request, response);
-    
-    out.println("<script>");
-    out.println("function deleteBoard() {");
-    out.println("  document.getElementById('form1').action = 'delete';");
-    out.println("}");
-    out.println("</script>");
-    
-    out.println("</body>");
-    out.println("</html>");
+      out.println("<!DOCTYPE html>");
+      out.println("<html>");
+      out.println("<head>");
+      out.println("  <meta charset='UTF-8'>");
+      out.println("  <title>게시판-상세정보</title>");
+      out.println("</head>");
+      out.println("<body>");
+      out.println("<h1>게시물 정보</h1>");
+      
+      if (board != null) {
+        out.println("<form id='form1' action='update' method='post'>");
+        out.println("<table border='1'>");
+        out.println("<tr>");
+        out.println("  <th>번호</th>");
+        out.printf("  <td><input type='text' name='no' value='%d' readonly></td>\n", 
+            board.getNo());
+        out.println("</tr>");
+        out.println("<tr>");
+        out.println("  <th>제목</th>");
+        out.printf("  <td><input type='text' name='title' value='%s'></td>\n", 
+            board.getTitle());
+        out.println("</tr>");
+        out.println("<tr>");
+        out.println("  <th>내용</th>");
+        out.printf("  <td><textarea rows='10' name='content' cols='60'>%s</textarea></td>\n", 
+            board.getContent());     
+        out.println("</tr>");
+        out.println("<tr>");
+        out.println("  <th>조회수</th>");
+        out.printf("  <td>%d</td>\n", 
+            board.getViews());
+        out.println("</tr>");
+        out.println("<tr>");
+        out.println("  <th>등록일</th>");
+        out.printf("  <td>%s</td>\n", 
+            board.getCreatedDate());
+        out.println("</tr>");
+        out.println("<tr>");
+        out.println("  <th>암호</th>");
+        out.println("  <td><input type='password' name='password'></td>");
+        out.println("</tr>");
+        out.println("</table>");
+        
+        out.println("<p>");
+        out.println("<button name='update' type='submit'>변경</button>");
+        out.println("<button name='delete' type='submit' onclick='deleteBoard()'>삭제</button>");
+        out.println("</p>");
+        
+        out.println("</form>");
+        
+      } else {
+        out.println("<p>해당 번호의 게시물을 찾을 수 없습니다.</p>");
+      }
+      RequestDispatcher rd = request.getRequestDispatcher("/copyright");
+      rd.include(request, response);
+      
+      out.println("<script>");
+      out.println("function deleteBoard() {");
+      out.println("  document.getElementById('form1').action = 'delete';");
+      out.println("}");
+      out.println("</script>");
+      
+      out.println("</body>");
+      out.println("</html>");
+      
+    } catch (Exception e) {
+      RequestDispatcher rd = request.getRequestDispatcher("/error");
+      request.setAttribute("error", e); 
+      rd.forward(request, response);
+    }   
   }
   
   // POST 요청이 들어오면 해당 게시물을 입력한 값으로 변경한다.
