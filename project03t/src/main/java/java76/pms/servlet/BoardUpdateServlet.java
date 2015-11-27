15796ae6454dfa3a3aa16e30c0f33a81a2dddb69
Jinyoung <jinyoung.eom@gmail.com>
2015-11-27 17:11:42 +0900

    .

--- project03t/src/main/java/java76/pms/servlet/BoardUpdateServlet.java
+++ project03t/src/main/java/java76/pms/servlet/BoardUpdateServlet.java
@@ -1,7 +1,6 @@
 package java76.pms.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -30,74 +29,13 @@ public class BoardUpdateServlet extends HttpServlet {
                                   .getAttribute("iocContainer");
       BoardDao boardDao = iocContainer.getBean(BoardDao.class);
       Board board = boardDao.selectOne(no);
+      request.setAttribute("board", board);
       
       response.setContentType("text/html;charset=UTF-8");
-      PrintWriter out = response.getWriter();
       
-      out.println("<!DOCTYPE html>");
-      out.println("<html>");
-      out.println("<head>");
-      out.println("  <meta charset='UTF-8'>");
-      out.println("  <title>게시판-상세정보</title>");
-      out.println("</head>");
-      out.println("<body>");
-      out.println("<h1>게시물 정보</h1>");
-      
-      if (board != null) {
-        out.println("<form id='form1' action='update' method='post'>");
-        out.println("<table border='1'>");
-        out.println("<tr>");
-        out.println("  <th>번호</th>");
-        out.printf("  <td><input type='text' name='no' value='%d' readonly></td>\n", 
-            board.getNo());
-        out.println("</tr>");
-        out.println("<tr>");
-        out.println("  <th>제목</th>");
-        out.printf("  <td><input type='text' name='title' value='%s'></td>\n", 
-            board.getTitle());
-        out.println("</tr>");
-        out.println("<tr>");
-        out.println("  <th>내용</th>");
-        out.printf("  <td><textarea rows='10' name='content' cols='60'>%s</textarea></td>\n", 
-            board.getContent());     
-        out.println("</tr>");
-        out.println("<tr>");
-        out.println("  <th>조회수</th>");
-        out.printf("  <td>%d</td>\n", 
-            board.getViews());
-        out.println("</tr>");
-        out.println("<tr>");
-        out.println("  <th>등록일</th>");
-        out.printf("  <td>%s</td>\n", 
-            board.getCreatedDate());
-        out.println("</tr>");
-        out.println("<tr>");
-        out.println("  <th>암호</th>");
-        out.println("  <td><input type='password' name='password'></td>");
-        out.println("</tr>");
-        out.println("</table>");
-        
-        out.println("<p>");
-        out.println("<button name='update' type='submit'>변경</button>");
-        out.println("<button name='delete' type='submit' onclick='deleteBoard()'>삭제</button>");
-        out.println("</p>");
-        
-        out.println("</form>");
-        
-      } else {
-        out.println("<p>해당 번호의 게시물을 찾을 수 없습니다.</p>");
-      }
-      RequestDispatcher rd = request.getRequestDispatcher("/copyright");
+      RequestDispatcher rd = request.getRequestDispatcher(
+                                          "/board/BoardDetail.jsp");
       rd.include(request, response);
-      
-      out.println("<script>");
-      out.println("function deleteBoard() {");
-      out.println("  document.getElementById('form1').action = 'delete';");
-      out.println("}");
-      out.println("</script>");
-      
-      out.println("</body>");
-      out.println("</html>");
       
     } catch (Exception e) {
       RequestDispatcher rd = request.getRequestDispatcher("/error");
@@ -129,26 +67,13 @@ public class BoardUpdateServlet extends HttpServlet {
         return;
       } 
       
+      // 오류 코드를 ServletRequest 보관소에 담는다.
+      request.setAttribute("errorCode", "401");
+
       response.setContentType("text/html;charset=UTF-8");
-      PrintWriter out = response.getWriter();
-      
-      out.println("<!DOCTYPE html>");
-      out.println("<html>");
-      out.println("<head>");
-      out.println("  <meta charset='UTF-8'>");
-      out.println("  <title>게시판-변경</title>");
-      out.println("</head>");
-      out.println("<body>");
-      out.println("<h1>게시물 변경 오류</h1>");
-      out.println("<p>해당 게시물이 존재하지 않거나 암호가 맞지 않습니다.</p>");
-      
-      RequestDispatcher rd = request.getRequestDispatcher("/copyright");
+      RequestDispatcher rd = request.getRequestDispatcher(
+          "/board/BoardAuthError.jsp");
       rd.include(request, response);
-      
-      out.println("</body>");
-      out.println("</html>");
-      
-      response.setHeader("Refresh", "2;url=list");
       
     } catch (Exception e) {
       RequestDispatcher rd = request.getRequestDispatcher("/error");
--- project03t/src/main/webapp/board/BoardList03.jsp
+++ project03t/src/main/webapp/board/BoardList03.jsp
@@ -1,9 +1,10 @@
 <%-- EL을 이용하여 게시물 데이터 출력하기 --%>
+<%@ page import="java76.pms.domain.Board"%>
+<%@ page import="java.util.List"%>
 <%@ page language="java" 
     contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"
     trimDirectiveWhitespaces="true"%>
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>    
 <!DOCTYPE html>
 <html>
 <head>
@@ -11,7 +12,7 @@
   <title>게시판-목록</title>
 </head>
 <body>
-<h1>게시판(with JSP + EL + JSTL)</h1>
+<h1>게시판(with JSP)</h1>
 <a href='form.html'>새 글</a><br>
 <table border='1'>
   <tr>
@@ -21,14 +22,21 @@
     <th>등록일</th>
   </tr>
 
-<c:forEach var="board" items="${boards}">
+<jsp:useBean id="boards" type="java.util.List<Board>" scope="request"/>
+<%
+for (Board board : boards) {
+  // EL은 로컬 변수를 사용할 수 없기 때문에, 일단 PageContext 보관소에 담는다.
+  // 목록에서 꺼낸 Board는 다른 JSP에서 사용할 것이 아니기 때문에,
+  // 굳이 ServletRequest나, HttpSession, ServletContext에 저장할 필요는 없다.
+  // 아니 해당 페이지에서만 사용될 데이터라면 절대 다른 보관소에 저장해서는 안된다.
+  pageContext.setAttribute("board", board);%>
   <tr>
     <td>${board.no}</td>
     <td><a href='update?no=${board.no}'>${board.title}</a></td>
     <td>${board.views}</td>
     <td>${board.createdDate}</td>
   </tr>
-</c:forEach>  
+<%}%>   
 </table>
 
 <jsp:include page="/Copyright.jsp"/>
