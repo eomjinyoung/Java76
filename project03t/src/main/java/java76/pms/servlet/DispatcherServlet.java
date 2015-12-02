d675dd310c9263b8ca682d542db97b24b05a17e9
Jinyoung <jinyoung.eom@gmail.com>
2015-12-02 14:50:18 +0900

    .

--- project03t/src/main/java/java76/pms/servlet/DispatcherServlet.java
+++ project03t/src/main/java/java76/pms/servlet/DispatcherServlet.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,7 +12,6 @@ import org.springframework.context.ApplicationContext;
 
 import java76.pms.controller.PageController;
 
-@WebServlet("*.do")
 public class DispatcherServlet extends HttpServlet {  
   private static final long serialVersionUID = 1L;
 
@@ -35,6 +33,7 @@ public class DispatcherServlet extends HttpServlet {
       String viewUrl = pageController.execute(request, response);
       
       //4) 페이지 컨트롤러가 리턴한 JSP를 실행한다.
+      response.setContentType("text/html;charset=UTF-8");
       RequestDispatcher rd = request.getRequestDispatcher(viewUrl);
       rd.include(request, response);
       
