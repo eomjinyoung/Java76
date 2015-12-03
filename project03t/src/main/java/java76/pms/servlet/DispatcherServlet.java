ed257b28d7985d762b928aa5e4501a6a6a3e5ebe
Jinyoung <jinyoung.eom@gmail.com>
2015-12-03 15:52:15 +0900

    .

--- project03t/src/main/java/java76/pms/servlet/DispatcherServlet.java
+++ project03t/src/main/java/java76/pms/servlet/DispatcherServlet.java
@@ -1,6 +1,8 @@
 package java76.pms.servlet;
 
 import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.Map;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -8,29 +10,34 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.context.ApplicationContext;
-
-import java76.pms.controller.PageController;
+import java76.pms.domain.RequestHandler;
 
 public class DispatcherServlet extends HttpServlet {  
   private static final long serialVersionUID = 1L;
 
+  @SuppressWarnings("unchecked")
   @Override
   public void service(
       HttpServletRequest request, HttpServletResponse response) 
       throws ServletException, IOException {
     try {
-      //1) 스프링 IoC 컨테이너 준비
-      ApplicationContext iocContainer = 
-          (ApplicationContext)this.getServletContext()
-                                  .getAttribute("iocContainer");
+      //1) 요청 핸들러 맵을 꺼낸다.
+      Map<String,RequestHandler> handlerMap = 
+          (Map<String,RequestHandler>)this.getServletContext()
+                                          .getAttribute("handlerMap");
       
       //2) 클라이언트 요청을 처리할 페이지 컨트롤러를 찾는다.
-      PageController pageController = 
-          (PageController)iocContainer.getBean(request.getServletPath());
+      RequestHandler requestHandler = 
+          handlerMap.get(request.getServletPath());
+      
+      if (requestHandler == null) { // 요청을 처리할 메서드를 못 찾았습니다.
+        throw new ServletException("해당 URL을 처리할 수 없습니다.");
+      }
       
       //3) 페이지 컨트롤러를 실행한다.
-      String viewUrl = pageController.execute(request, response);
+      Object instance = requestHandler.getInstance();
+      Method method = requestHandler.getMethod();
+      String viewUrl = (String)method.invoke(instance, request, response);
       
       //4) 페이지 컨트롤러가 리턴한 JSP를 실행한다.
       if (viewUrl.startsWith("redirect:")) {
