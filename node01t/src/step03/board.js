136dc5e3dbad95f5ed8060d901a672f43b214bdd
Jinyoung <jinyoung.eom@gmail.com>
2016-01-22 16:28:58 +0900

    .

--- node01t/src/step03/board.js
+++ node01t/src/step03/board.js
@@ -1,8 +1,10 @@
-// DB 커넥션풀 적용하기 
+// 게시물 변경 기능 추가
+// + POST 요청 파라미터 값 추출하는 방법
 var http = require('http');
 var mysql = require('mysql');
 var url = require('url');
 var dateFormat = require('dateformat');
+var qs = require('querystring');
 
 var pool  = mysql.createPool({
   connectionLimit : 10,
@@ -78,7 +80,7 @@ handlerMap["/board/detail.do"] = function(request, response) {
 		  response.write("</head>\n");
 		  response.write("<body>\n");
 		  response.write("<h1>게시물 상세정보</h1>\n");
-			
+		  response.write("<form action='update.do' method='post'>\n");	
 		  response.write("<table border='1'>\n");
 		  response.write("<tr>\n");
 		  response.write("  <th>번호</th>\n");
@@ -87,12 +89,12 @@ handlerMap["/board/detail.do"] = function(request, response) {
 		  response.write("</tr>\n");
 		  response.write("<tr>\n");
 		  response.write("  <th>제목</th>\n");
-		  response.write("  <td><input type='text' name='no' value='"
+		  response.write("  <td><input type='text' name='title' value='"
 				  + rows[0].title + "'></td>\n");
 		  response.write("</tr>\n");
 		  response.write("<tr>\n");
 		  response.write("  <th>내용</th>\n");
-		  response.write("  <td><textarea rows='5' cols='50'>" 
+		  response.write("  <td><textarea name='content' rows='5' cols='50'>" 
 				  + rows[0].content + "</textarea></td>\n");
 		  response.write("</tr>\n");
 		  response.write("<tr>\n");
@@ -105,9 +107,12 @@ handlerMap["/board/detail.do"] = function(request, response) {
 				  + dateFormat(rows[0].cre_dt,"yyyy-mm-dd") + "</td>\n");
 		  response.write("</tr>\n");
 		  
-		  response.write("</table>");
-		  response.write("<p><a href='delete.do?no=" 
+		  response.write("</table>\n");
+		  response.write("<p>"
+				  + "<input type='submit' value='변경'> "
+				  + "<a href='delete.do?no=" 
 				  + rows[0].bno + "'>삭제</a><p>\n");
+		  response.write("</form>\n");
 		  response.write("</body>\n");
 		  response.write("</html>\n");
 		  response.end();
@@ -126,6 +131,34 @@ handlerMap["/board/delete.do"] = function(request, response) {
 			  'Location' : '/board/list.do' 
 		  });
 		  response.end();
+	});
+};
+
+handlerMap["/board/update.do"] = function(request, response) {
+	var urlInfo = url.parse(request.url, true);
+	
+	var message = '';
+	
+	// "data" 이벤트 => POST 요청 시,
+	//                message-body 영역의 데이터를 읽을 때 발생하는 이벤트 
+	request.on('data', function(data) {
+		message += data;
+	});
+	
+	// "end" 이벤트 => POST 요청 시, 
+	//                message-body 영역의 데이터를 모두 읽었을 때 발생하는 이벤트
+	request.on('end', function() {
+		var params = qs.parse(message);
+		pool.query(
+		  "update board set title=?, content=? where bno=?", 
+		  [params.title,params.content,params.no], 
+		  function(err, rows, fields) {
+			  if (err) throw err;
+			  response.writeHead(302, {
+				  'Location' : '/board/list.do' 
+			  });
+			  response.end();
+		});
 	});
 };
 
